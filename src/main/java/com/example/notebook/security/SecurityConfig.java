5ea9cfb39f729bfa05352cea164117f43b81983a
sokol <sokolplus3783@gmail.com>
2023-05-31 19:11:46 +0300

    permit all addresses SecurityConfig

--- src/main/java/com/example/notebook/security/SecurityConfig.java
+++ src/main/java/com/example/notebook/security/SecurityConfig.java
@@ -1,35 +1,39 @@
 package com.example.notebook.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.notebook.entity.Account;
+import com.example.notebook.repository.AccountRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web
-    .configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-
-  @Autowired
-  private UserDetailsService userDetailsService;
 
   @Bean
   public PasswordEncoder encoder() {
     return new BCryptPasswordEncoder();
   }
 
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth)
-      throws Exception {
-
-    auth
-        .userDetailsService(userDetailsService)
-        .passwordEncoder(encoder());
-
+  @Bean
+  public UserDetailsService userDetailsService(AccountRepository userRepo) {
+    return username -> {
+      Account user = userRepo.findByLogin(username);
+      if (user != null) {
+        return user;
+      }
+      throw new UsernameNotFoundException("User ‘" + username + "’ not found");
+    };
   }
 
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    return http.authorizeHttpRequests(authorize -> authorize
+            .requestMatchers("/", "/**").permitAll()
+        ).build();
+  }
 }
