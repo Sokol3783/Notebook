a89e1b8d3ead3e3a23ec2f04dd8d3e2fd7a9056e
Sokol3783 <sokolplus3783@gmail.com>
2023-06-13 23:05:44 +0300

    refactor SecurityConfig

--- src/main/java/com/example/notebook/security/SecurityConfig.java
+++ src/main/java/com/example/notebook/security/SecurityConfig.java
@@ -1,15 +1,11 @@
 package com.example.notebook.security;
 
-import com.example.notebook.entity.Account;
-import com.example.notebook.repository.AccountRepository;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,17 +17,6 @@ public class SecurityConfig {
   @Bean
   public PasswordEncoder encoder() {
     return new BCryptPasswordEncoder();
-  }
-
-  @Bean
-  public UserDetailsService userDetailsService(AccountRepository userRepo) {
-    return username -> {
-      Account user = userRepo.findByLogin(username);
-      if (user != null) {
-        return user;
-      }
-      throw new UsernameNotFoundException("User ‘" + username + "’ not found");
-    };
   }
 
   @Bean
