5dcd0fada33d77b445a0e746ad4099789357c74f
Sokol3783 <sokolplus3783@gmail.com>
2023-06-14 19:31:17 +0300

    working over authorization

--- src/main/java/com/example/notebook/controllers/web/HomeController.java
+++ src/main/java/com/example/notebook/controllers/web/HomeController.java
@@ -1,12 +1,14 @@
 package com.example.notebook.controllers.web;
 
+import com.example.notebook.dto.LoginDTO;
+import com.example.notebook.entity.Account;
 import com.example.notebook.security.AccountDetailsService;
-import org.springframework.security.core.userdetails.UserDetails;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -23,19 +25,23 @@ public class HomeController {
   }
 
   @GetMapping(value = "/home")
-  public String home() {
+  public String home(Model model) {
+    if (model.containsAttribute("loginDTO")) {
+      LoginDTO loginDTO = (LoginDTO) model.getAttribute("loginDTO");
+      loginDTO.setPassword(null);
+      model.addAttribute(loginDTO);
+    }
     return "home";
   }
 
-  @PostMapping(value = "/home", params = {"login", "pass"})
-  public String login(Model model,
-      @RequestParam("login") String username,
-      @RequestParam("pass") CharSequence password) {
-    UserDetails user = service.authorizeUser(username, password);
-    if (user.isEnabled()) {
+  @PostMapping(value = "/home")
+  public String login(HttpSession session, @ModelAttribute("loginDTO") LoginDTO loginDTO) {
+    Account account = service.authorizeUser(loginDTO.getLogin(), loginDTO.getPassword().toString());
+    if (account.isEnabled()) {
+      session.setAttribute("account", account);
       return "redirect:/pages/notebook/";
     }
-    model.addAttribute("pass");
-    return model.toString();
+    return "/home";
   }
+
 }
