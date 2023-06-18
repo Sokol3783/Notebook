04fd3472d2c7cacc3d37ed8186f51c84aef7b7a8
Sokol3783 <sokolplus3783@gmail.com>
2023-06-19 01:54:24 +0300

    inject account id

--- src/main/java/com/example/notebook/security/SecurityConfig.java
+++ src/main/java/com/example/notebook/security/SecurityConfig.java
@@ -1,14 +1,22 @@
 package com.example.notebook.security;
 
+import com.example.notebook.entity.Account;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -35,11 +43,24 @@ public class SecurityConfig {
         .formLogin((form) -> form
             .loginPage("/a/home")
             .loginProcessingUrl("/a/home")
+            .successHandler(getHandler())
             .defaultSuccessUrl("/u/notebook")
             .permitAll())
-        .logout( s -> s.invalidateHttpSession(true)
+        .logout(s -> s.invalidateHttpSession(true)
             .logoutSuccessUrl("/a/home"))
         .build();
+  }
+
+  private AuthenticationSuccessHandler getHandler() {
+    return new AuthenticationSuccessHandler() {
+      @Override
+      public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+          Authentication authentication) throws IOException, ServletException {
+        Account account = (Account) authentication.getPrincipal();
+        HttpSession session = request.getSession();
+        session.setAttribute("id", account.getId());
+      }
+    };
   }
 
 }
